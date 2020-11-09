97a6b161438346ad9c289eab3d9df6a819efa51f
Hsin Yi, Chen <ossug.hychen@gmail.com>
2020-11-09 16:20:43 +0800

    SAVEPOINT

--- challenge#10-erc20-contract/erc20/lib.rs
+++ challenge#10-erc20-contract/erc20/lib.rs
@@ -3,44 +3,130 @@
 use ink_lang as ink;
 
 #[ink::contract]
-mod flipper {
+mod erc20 {
+    #[cfg(not(feature = "ink-as-dependency"))]
+    use ink_storage::{
+        collections::HashMap as StorageHashMap,
+        lazy::Lazy,
+    };
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
-    pub struct Flipper {
+    pub struct ERC20 {
         /// Stores a single `bool` value on the storage.
-        value: bool,
+        total_supply: Lazy<Balance>,
+        balances: StorageHashMap<AccountId, Balance>,
+        allowances: StorageHashMap<(AccountId, AccountId), Balance>
     }
 
-    impl Flipper {
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        #[ink(topic)]
+        value: Balance
+    }
+
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        #[ink(topic)]
+        value: Balance
+    }
+
+    /// The ERC-20 error types.
+    #[derive(Debug, PartialEq, Eq, scale::Encode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        /// Returned if not enough balance to fulfill a request is available.
+        InsufficientBalance,
+        /// Returned if not enough allowance to fulfill a request is available.
+        InsufficientAllowance,
+    } 
+
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    impl ERC20 {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(init_supply: Balance) -> Self {
+            let caller = Self::env().caller();
+            let mut balances = StorageHashMap::new();
+            balances.insert(caller, init_supply);
+            let instance = Self {
+                total_supply: Lazy::new(init_supply),
+                balances: balances,
+                allowances: StorageHashMap::new()
+            };
+            Self::env().emit_event(Transfer {
+                from: None,
+                to: Some(caller),
+                value: init_supply
+            });
+            instance
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn total_supply(&self) -> Balance {
+            *self.total_supply
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            self.balances.get(&owner).copied().unwrap_or(0)
+        }
+
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            self.allowances.get(&(owner, spender)).copied().unwrap_or(0)
+        }
+
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> Result<()> {
+            let owner = self.env().caller();
+            self.allowances.insert((owner, spender), value);
+            self.env().emit_event(Approval {
+                owner,
+                spender,
+                value
+            });
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, _to: AccountId, value: Balance) -> Result<()> {
+            let _from = self.env().caller();
+            self.transfer_from_to(_from, _to, value);
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn transfer_from_to(
+            &mut self, 
+            _from: AccountId, 
+            _to: AccountId,
+            value: Balance
+        ) -> Result<()> {
+            let from_balances = self.balance_of(_from);
+            if from_balances < value {
+                return Err(Error::InsufficientBalance);
+            }
+            self.balances.insert(_from, from_balances - value);
+            let to_balances = self.balance_of(_to);
+            self.balances.insert(_to, to_balances + value);
+            self.env().emit_event(Transfer {
+                from: Some(_from),
+                to: Some(_to),
+                value,
+            });
+            Ok(())
         }
     }
 
@@ -51,21 +137,123 @@ mod flipper {
     mod tests {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
+        use ink_env::{
+            hash::{
+                Blake2x256,
+                CryptoHash,
+                HashOutput,
+            },
+            Clear,
+        };
+        type Event = <ERC20 as ::ink_lang::BaseEvent>::Type;
+        use ink_lang as ink;
+
+        fn assert_transfer_event(
+            event: &ink_env::test::EmittedEvent,
+            expected_from: Option<AccountId>,
+            expected_to: Option<AccountId>,
+            expected_value: Balance,
+        ) {
+            let decoded_event = <Event as scale::Decode>::decode(&mut &event.data[..])
+                .expect("encountered invalid contract event data buffer");
+            if let Event::Transfer(Transfer { from, to, value }) = decoded_event {
+                assert_eq!(from, expected_from, "encountered invalid Transfer.from");
+                assert_eq!(to, expected_to, "encountered invalid Transfer.to");
+                assert_eq!(value, expected_value, "encountered invalid Trasfer.value");
+            } else {
+                panic!("encountered unexpected event kind: expected a Transfer event")
+            }
+            fn encoded_into_hash<T>(entity: &T) -> Hash
+            where
+                T: scale::Encode,
+            {
+                let mut result = Hash::clear();
+                let len_result = result.as_ref().len();
+                let encoded = entity.encode();
+                let len_encoded = encoded.len();
+                if len_encoded <= len_result {
+                    result.as_mut()[..len_encoded].copy_from_slice(&encoded);
+                    return result
+                }
+                let mut hash_output =
+                    <<Blake2x256 as HashOutput>::Type as Default>::default();
+                <Blake2x256 as CryptoHash>::hash(&encoded, &mut hash_output);
+                let copy_len = core::cmp::min(hash_output.len(), len_result);
+                result.as_mut()[0..copy_len].copy_from_slice(&hash_output[0..copy_len]);
+                result
+            }
+            let expected_topics = vec![
+                encoded_into_hash(b"ERC20::Transfer"),
+                encoded_into_hash(&expected_from),
+                encoded_into_hash(&expected_to),
+                encoded_into_hash(&expected_value),
+            ];
+            for (n, (actual_topic, expected_topic)) in
+                event.topics.iter().zip(expected_topics).enumerate()
+            {
+                let topic = actual_topic
+                    .decode::<Hash>()
+                    .expect("encountered invalid topic encoding");
+                assert_eq!(topic, expected_topic, "encountered invalid topic at {}", n);
+            }
+        }
 
         /// We test if the default constructor does its job.
-        #[test]
-        fn default_works() {
-            let flipper = Flipper::default();
-            assert_eq!(flipper.get(), false);
+        #[ink::test]
+        fn new_works() {
+            let _erc20 = ERC20::new(100);
+            let emitted_events = ink_env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(1, emitted_events.len());
+
+            assert_transfer_event(
+                &emitted_events[0],
+                None,
+                Some(AccountId::from([0x01; 32])),
+                100,
+            );
         }
 
-        /// We test a simple use case of our contract.
-        #[test]
-        fn it_works() {
-            let mut flipper = Flipper::new(false);
-            assert_eq!(flipper.get(), false);
-            flipper.flip();
-            assert_eq!(flipper.get(), true);
+        #[ink::test]
+        fn balance_of_works() {
+            let _erc20 = ERC20::new(100);
+            let accounts =
+                ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                    .expect("Cannot get accounts");
+            assert_eq!(_erc20.balance_of(accounts.alice), 100);
+            assert_eq!(_erc20.balance_of(accounts.bob), 0);
         }
+
+        #[ink::test]
+        fn transfer_from_to_works() {
+            let mut _erc20 = ERC20::new(100);
+            let accounts =
+                ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                    .expect("Cannot get accounts");
+
+            // alice transfer 49 tokens to bob.
+            assert_eq!(_erc20.transfer_from_to(accounts.alice, accounts.bob, 49), Ok(()));
+            // alices has 51 tokens.
+            assert_eq!(_erc20.balance_of(accounts.alice), 51);
+            // bob has 49 tokens.
+            assert_eq!(_erc20.balance_of(accounts.bob), 49);
+
+            let emitted_events = ink_env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(emitted_events.len(), 2);
+            // Check first transfer event related to ERC-20 instantiation.
+            assert_transfer_event(
+                &emitted_events[0],
+                None,
+                Some(AccountId::from([0x01; 32])),
+                100,
+            );
+            // Check the second transfer event relating to the actual trasfer.
+            assert_transfer_event(
+                &emitted_events[1],
+                Some(AccountId::from([0x01; 32])),
+                Some(AccountId::from([0x02; 32])),
+                49,
+            );
+        }
+
     }
 }
